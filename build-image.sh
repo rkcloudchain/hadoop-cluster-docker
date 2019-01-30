a8d9705f19302500b187c0ba1bc394e02ed21737
Xu Qiaolun <jamesxql@gmail.com>
2019-01-30 13:15:13 +0800

    Change docker image tag

--- build-image.sh
+++ build-image.sh
@@ -3,6 +3,6 @@
 echo ""
 
 echo -e "\nbuild docker hadoop image\n"
-sudo docker build -t reg.querycap.com/cloudchain/hadoop:1.0 .
+sudo docker build -t reg.querycap.com/cloudchain/hadoop:1.1 .
 
 echo ""
