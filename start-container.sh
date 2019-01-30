8a0c5f8fbf1dcf49ab5e64a39edd15a29f724a4f
Xu Qiaolun <jamesxql@gmail.com>
2019-01-30 13:32:34 +0800

    Don't export 8088 port

--- start-container.sh
+++ start-container.sh
@@ -11,7 +11,6 @@ sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
 				-p 9000:9000 \
-                -p 8088:8088 \
 				-v /data/hdfs/namenode:/root/hdfs/namenode \
                 --name hadoop-master \
                 --hostname hadoop-master \
